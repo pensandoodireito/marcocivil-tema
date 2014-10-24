e4ca555db84067e284aed1672e4500177a461531
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2014-10-24 19:06:09 -0200

    atualização do código gerado pelo Thiago Sousa com o restante das páginas

--- js/core-mci.js
+++ js/core-mci.js
@@ -1,3 +1,14 @@
-$(window).load(function(){
-	$("#content-mci").css('height', $(document).height());
-});
+//Initial load of page
+$(document).ready(sizeContent);
+
+//Every resize of window
+$(window).resize(sizeContent);
+
+//Dynamically assign height
+function sizeContent() {
+    var newHeight = $("html").height() - $(".header").height() - $(".footer").height() + "px";
+    $("#content-mci").css("height", newHeight);
+}
+
+// Refresh na tela ao redimensionar
+$(window).resize(function(){location.reload();});
