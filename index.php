ac2a77878eab9b9b9e3a6aefe3c3369431ee7c6e
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-01-17 18:03:58 -0200

    Alteração de pastas do tema, inclusão de HTMLs

--- index.php
+++ index.php
@@ -1,6 +1,128 @@
 <?php get_header(); ?>
-        <div class="row">
-            <?php get_template_part('logo', 'mci'); ?>
-            <?php get_template_part('menu','interno'); ?>
+<div class="conteudo" id="marco-civil">
+        <div class="container">
+            <div class="apresenta-protecao">
+                <div class="row">
+                    <div class="col-md-4 text-center">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/marcocivil-w.png" class="img-adptive" alt="Logo: Marco Civil da Internet">
+                    </div>
+                    <div class="col-md-8 white">
+                        <h2 class="font-amatic">Marco Civil: o que é?</h2>
+                        <p>O Marco Civil da Internet, Lei 12.965/14, estabelece princípios, garantias, direitos e deveres dos usuários da Internet no Brasil, sendo a primeira lei construída de forma colaborativa entre Governo e sociedade utilizando a internet como plataforma de debate.</p>
+                    </div>
+                </div>
+                <div class="assinatura white">
+                    <p class="text-center fontsize-sm">Realização:</p>
+                    <div class="clearfix text-center fontsize-sm mt-md">
+                        <div class="col-sm-4">
+                            <p class="text-center">
+                                Secretaria de
+                                <br>
+                                <strong>Assuntos Legislativos</strong>
+                            </p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p class="text-center">
+                                Secretaria
+                                <br>
+                                <strong>Nacional do Consumidor</strong>
+                            </p>
+                        </div>
+                        <div class="col-sm-4">
+                            <p class="text-center">
+                                <strong>Participa.br</strong>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="temas clearfix">
+                <div class="col-sm-4">
+                    <div class="titulo">
+                        <h3 class="font-amatic blue text-center">Temas</h3>
+                        <div class="fontsize-sm">
+                            <p>Serão vinte e cinco artigos, divididos em cinco capítulos:</p>
+                            <p><i class="fa fa-caret-right"></i> Disposições preliminares;</p>
+                            <p><i class="fa fa-caret-right"></i> Dos direitos e garantias dos usuários; </p>
+                            <p><i class="fa fa-caret-right"></i> Da provisão de conexão e aplicações da Internet;</p>
+                            <p><i class="fa fa-caret-right"></i> Da atuação do poder público;</p>
+                            <p><i class="fa fa-caret-right"></i> Disposições Finais.</p>
+                        </div>
+
+                    </div>
+                    <div class="mt-xl text-center">
+                        <button type="button" class="btn btn-danger btn-lg font-roboto">Participe do debate!</button>
+                    </div>
+                </div>
+                <div class="col-sm-8">
+                    <div class="clearfix mt-lg">
+                        <div class="col-sm-3 text-center">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/marcocivil-tema-01.jpg" class="img-adptive" alt="">
+                        </div>
+                        <div class="col-sm-8">
+                            <h4 class="font-amatic blue">Registros de acesso</h4>
+                            <p>Os provedores de internet e de serviços só serão obrigados a fornecer informações dos usuários se receberem ordem judicial.</p>
+                        </div>
+                    </div>
+                    <div class="clearfix mt-lg">
+                        <div class="col-sm-3 text-center">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/marcocivil-tema-02.jpg" class="img-adptive" alt="">
+                        </div>
+                        <div class="col-sm-8">
+                            <h4 class="font-amatic blue">Apuração de infrações à Lei</h4>
+                            <p>Significa que todas as informações que trafegam na rede devem ser tratadas da mesma forma, navegando à mesma velocidade, ou seja, na velocidade da contratação.</p>
+                        </div>
+                    </div>
+                    <div class="clearfix mt-lg">
+                        <div class="col-sm-3 text-center">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/marcocivil-tema-03.jpg" class="img-adptive" alt="">
+                        </div>
+                        <div class="col-sm-8">
+                            <h4 class="font-amatic blue">Proteção de dados dessoais</h4>
+                            <p>Os provedores de internet e de serviços só serão obrigados a fornecer informações dos usuários se receberem ordem judicial.</p>
+                        </div>
+                    </div>
+                    <div class="clearfix mt-lg">
+                        <div class="col-sm-3 text-center">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/marcocivil-tema-04.jpg" class="img-adptive" alt="">
+                        </div>
+                        <div class="col-sm-8">
+                            <h4 class="font-amatic blue">Políticas públicas de internet</h4>
+                            <p>Os provedores de internet e de serviços só serão obrigados a fornecer informações dos usuários se receberem ordem judicial.</p>
+                        </div>
+                    </div>
+                    <div class="clearfix mt-lg">
+                        <div class="col-sm-3 text-center">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/marcocivil-tema-05.jpg" class="img-adptive" alt="">
+                        </div>
+                        <div class="col-sm-8">
+                            <h4 class="font-amatic blue">Neutralidade</h4>
+                            <p>Os provedores de internet e de serviços só serão obrigados a fornecer informações dos usuários se receberem ordem judicial.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="detalhes-marco white">
+            <div class="col-md-8">
+                <h3 class="font-amatic">Conheça mais sobre a Lei</h3>
+                <p>O Marco Civil da Internet, Lei 12.965/14, estabelece princípios, garantias, direitos e deveres dos usuários da Internet no Brasil, sendo a primeira lei construída de forma colaborativa entre Governo e sociedade utilizando a internet como plataforma de debate.</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum, lacus vitae eleifend aliquet, leo lacus consequat elit, sed sollicitudin mauris massa vel erat. </p>
+
+
+                <p>Quisque ultricies laoreet orci. Etiam eros elit, consectetur a enim nec, dapibus placerat neque. Duis sed erat urna. Fusce auctor elit vitae massa posuere tempor. Nunc elementum sollicitudin felis sed euismod. Nam vulputate, arcu porta pretium lacinia, est lorem gravida massa, ac faucibus quam neque nec dui. Duis accumsan rutrum dolor, a lacinia erat gravida non</p>
+                </div>
+                <div class="col-md-4 text-center">
+                   <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-comp.jpg" class="img-adptive" alt="">
+                </div>
+            </div>
+
+        </div>
+</div>
+ <div class="container mt-md">
+            <?php get_template_part('mini-tutorial'); ?>
         </div>
 <?php get_footer(); ?>
--- logo-mci.php
+++
@@ -1,3 +0,0 @@
-<div id="logo-mci">
-    <a href="<?php echo get_site_url(); ?>"><h2>Marco Civil da Internet</h2></a>
-</div>
