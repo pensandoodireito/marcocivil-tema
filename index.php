9dadedad97295bbd8926259d2a7bf1bf22355b89
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-01-19 22:54:39 -0200

    Alteracoes simples

--- index.php
+++ index.php
@@ -5,92 +5,76 @@
             <div class="row">
                 <div class="col-md-4 text-center">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/marcocivil-w.png" class="img-adptive"
-                         alt="Logo: Marco Civil da Internet">
+                    alt="Logo: Marco Civil da Internet">
                 </div>
                 <div class="col-md-8 white">
                     <h2 class="font-amatic">Marco Civil: o que é?</h2>
-
                     <p>O Marco Civil da Internet, Lei 12.965/14, estabelece princípios, garantias, direitos e deveres
-                        dos usuários da Internet no Brasil, sendo a primeira lei construída de forma colaborativa entre
-                        Governo e sociedade utilizando a internet como plataforma de debate.</p>
+                    dos usuários da Internet no Brasil, sendo a primeira lei construída de forma colaborativa entre
+                    Governo e sociedade utilizando a internet como plataforma de debate.</p>
                 </div>
             </div>
         </div>
-
     </div>
-    <div class="temas clearfix">
-        <div class="col-sm-4">
-            <div class="titulo">
-                <h3 class="font-amatic blue text-center">Temas</h3>
-
-                <div class="fontsize-sm">
-                    <p>Serão vinte e cinco artigos, divididos em cinco capítulos:</p>
-
-                    <p><i class="fa fa-caret-right"></i> Disposições preliminares;</p>
-
-                    <p><i class="fa fa-caret-right"></i> Dos direitos e garantias dos usuários; </p>
-
-                    <p><i class="fa fa-caret-right"></i> Da provisão de conexão e aplicações da Internet;</p>
-
-                    <p><i class="fa fa-caret-right"></i> Da atuação do poder público;</p>
-
-                    <p><i class="fa fa-caret-right"></i> Disposições Finais.</p>
+    <div class="container">
+        <div class="temas clearfix">
+            <div class="col-sm-4">
+                <div class="titulo">
+                    <h3 class="font-amatic blue text-center">Temas</h3>
+                    <div class="fontsize-sm">
+                        <p>Serão vinte e cinco artigos, divididos em cinco capítulos:</p>
+                        <p><i class="fa fa-caret-right"></i> Disposições preliminares;</p>
+                        <p><i class="fa fa-caret-right"></i> Dos direitos e garantias dos usuários; </p>
+                        <p><i class="fa fa-caret-right"></i> Da provisão de conexão e aplicações da Internet;</p>
+                        <p><i class="fa fa-caret-right"></i> Da atuação do poder público;</p>
+                        <p><i class="fa fa-caret-right"></i> Disposições Finais.</p>
+                    </div>
                 </div>
-
-            </div>
-            <div class="mt-xl text-center">
-                <button type="button" class="btn btn-danger btn-lg font-roboto">Participe do debate!</button>
-            </div>
-        </div>
-        <div class="col-sm-8">
-            <?php
-            $temas = get_terms( 'tema', 'hide_empty=0' );
-
-            foreach($temas as $tema): ?>
-            <div class="clearfix mt-lg">
-                <div class="col-sm-3 text-center">
-                    <a href="<?php echo get_term_link($tema); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/<?php echo $tema->slug; ?>.jpg"
-                         class="img-adptive" alt=""></a>
+                <div class="mt-xl text-center">
+                    <button type="button" class="btn btn-danger btn-lg font-roboto">Participe do debate!</button>
                 </div>
-                <div class="col-sm-8">
-                    <h4 class="font-amatic blue">
+            </div>
+            <div class="col-sm-8">
+                <?php
+                $temas = get_terms( 'tema', 'hide_empty=0' );
+                foreach($temas as $tema): ?>
+                <div class="clearfix mt-lg">
+                    <div class="col-sm-3 text-center">
+                        <a href="<?php echo get_term_link($tema); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/<?php echo $tema->slug; ?>.jpg"
+                        class="img-adptive" alt=""></a>
+                    </div>
+                    <div class="col-sm-8">
+                        <h4 class="font-amatic blue">
                         <a href="<?php echo get_term_link($tema); ?>"><?php echo $tema->name; ?></a>
-                    </h4>
-
-<!--                    <p>Os provedores de internet e de serviços só serão obrigados a fornecer informações dos usuários se-->
-<!--                        receberem ordem judicial.</p>-->
+                        </h4>
+                        <!--                    <p>Os provedores de internet e de serviços só serão obrigados a fornecer informações dos usuários se-->
+                        <!--                        receberem ordem judicial.</p>-->
+                    </div>
                 </div>
+                <?php endforeach; ?>
             </div>
-            <?php endforeach; ?>
         </div>
-
-
     </div>
-    <div class="detalhes-marco white">
-        <div class="col-md-8">
-            <h3 class="font-amatic">Conheça mais sobre a Lei</h3>
-
-            <p>O Marco Civil da Internet, Lei 12.965/14, estabelece princípios, garantias, direitos e deveres dos
+    <div class="container">
+        <div class="detalhes-marco white">
+            <div class="col-md-8">
+                <h3 class="font-amatic">Conheça mais sobre a Lei</h3>
+                <p>O Marco Civil da Internet, Lei 12.965/14, estabelece princípios, garantias, direitos e deveres dos
                 usuários da Internet no Brasil, sendo a primeira lei construída de forma colaborativa entre Governo e
                 sociedade utilizando a internet como plataforma de debate.</p>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum, lacus vitae eleifend aliquet, leo
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum, lacus vitae eleifend aliquet, leo
                 lacus consequat elit, sed sollicitudin mauris massa vel erat. </p>
-
-
-            <p>Quisque ultricies laoreet orci. Etiam eros elit, consectetur a enim nec, dapibus placerat neque. Duis sed
+                <p>Quisque ultricies laoreet orci. Etiam eros elit, consectetur a enim nec, dapibus placerat neque. Duis sed
                 erat urna. Fusce auctor elit vitae massa posuere tempor. Nunc elementum sollicitudin felis sed euismod.
                 Nam vulputate, arcu porta pretium lacinia, est lorem gravida massa, ac faucibus quam neque nec dui. Duis
                 accumsan rutrum dolor, a lacinia erat gravida non</p>
-        </div>
-        <div class="col-md-4 text-center">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-comp.jpg" class="img-adptive" alt="">
+            </div>
+            <div class="col-md-4 text-center">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-comp.jpg" class="img-adptive" alt="">
+            </div>
         </div>
     </div>
-
 </div>
-</div>
-<div class="container mt-md">
-    <?php get_template_part('mini-tutorial'); ?>
-</div>
+<?php get_template_part('mini-tutorial'); ?>
+<?php get_template_part('noticias'); ?>
 <?php get_footer(); ?>
