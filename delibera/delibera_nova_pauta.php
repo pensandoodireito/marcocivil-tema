9f55eaca095b2e6418b62937a3add827abcea131
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-01-25 23:39:07 -0200

    Alteração da página de nova pauta, e no css/theme-debates-marco.css que corrige ela;

--- delibera/delibera_nova_pauta.php
+++ delibera/delibera_nova_pauta.php
@@ -1,25 +1,27 @@
-<?php 
+<?php
 global $deliberaThemes;
-                
+
 get_header();
 ?>
-
-<div id="delibera">
-    <div id="container">
-    	<div id="content" role="main">
-    		<?php
-
-    		// Chama o cabeçalho que apresenta o sistema de discussão
-    		get_delibera_header();
-		
-            // chama o formulário de nova pauta
-            if (is_user_logged_in()) {
+<div class="container">
+    <div id="delibera">
+        <div id="container">
+            <div id="content" role="main">
+                <?php
+                // Chama o cabeçalho que apresenta o sistema de discussão
+                get_delibera_header();
+                
+                // chama o formulário de nova pauta
+                if (is_user_logged_in()) {
                 include $deliberaThemes->themeFilePath('form-nova-pauta.php');
-            }
-        
-    		?>
-    	</div><!-- #content -->
-    </div><!-- #container -->
-</div>
+                }
+                
+                ?>
+                </div><!-- #content -->
+                </div><!-- #container -->
+            </div>
+        </div>
 
-<?php get_footer(); ?>
+
+
+        <?php get_footer(); ?>
