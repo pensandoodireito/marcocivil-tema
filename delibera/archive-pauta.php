f37b0961c5ea04dd73515c7daf36790c29100631
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-01-27 16:02:50 -0200

    Alteração no filtro de listagem de pautas;

--- delibera/archive-pauta.php
+++ delibera/archive-pauta.php
@@ -11,7 +11,7 @@ $options = delibera_get_config();
 $tema = $wp_query->get_queried_object();
 $archive_tema = false;
 if ( is_object($tema) && property_exists($tema, 'taxonomy') && $tema->taxonomy == "tema" ) {
-    $archive_tema = true;
+$archive_tema = true;
 }
 ?>
 <div class="conteudo" id="marco-civil">
@@ -21,47 +21,66 @@ if ( is_object($tema) && property_exists($tema, 'taxonomy') && $tema->taxonomy =
                 <?php get_template_part("menu", "horizontal"); ?>
             </div>
         </div>
+        <div>
+            <?php if ($archive_tema): ?>
+            <h4 class="red font-roboto"><strong>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/<?php echo $tema->slug; ?>.png" class="img-adptive" alt="">
+            <?php echo $tema->name; ?>
+            </strong>
+            </h4>
+            <?php
+            echo $tema->description;
+            else:
+            get_delibera_header(); ?>
+        </div>
         <div class="row">
-            <div class="col-xs-12">
-                
-                <?php if ($archive_tema): ?>
-                <h3> <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/<?php echo $tema->slug; ?>.png" class="img-adptive" alt="">
-                <?php echo $tema->name; ?> </h3>
-                <?php
-                echo $tema->description;
-                else:
-                get_delibera_header(); ?>
-
-
-                <h2>Filtrar Pautas</h2>
-                <form>
-                    <ul class="status">
-                        <?php foreach (get_terms('situacao') as $situacao) : ?>
-                        <li>
-                            <span
-                            class="<?php echo (isset($_GET['situacao_filtro'][$situacao->slug]) && $_GET['situacao_filtro'][$situacao->slug] == 'on') ? 'selected' : ''; ?>">
-                            <?php echo $situacao->name; ?>
-                            <input type="hidden" name="situacao_filtro[<?php echo $situacao->slug; ?>]"
-                            value="<?php echo (isset($_GET['situacao_filtro'][$situacao->slug]) && $_GET['situacao_filtro'][$situacao->slug] == 'on') ? 'on' : ''; ?>"/>
-                            </span>
-                        </li>
-                        <?php endforeach; ?>
-                    </ul>
-                    <ul>
-                        <?php foreach (get_terms('tema') as $tema) : ?>
-                        <li><label class="checkbox"><input type="checkbox"
-                            name="tema_filtro[<?php echo $tema->slug; ?>]" <?php echo (isset($_GET['tema_filtro'][$tema->slug]) && $_GET['tema_filtro'][$tema->slug] == 'on') ? ' checked="checked" ' : ''; ?> /><?php echo $tema->name; ?>
-                        </label></li>
-                        <?php endforeach; ?>
-                    </ul>
-                    <div class="textright">
-                        <button type="submit" class="btn">Filtrar</button>
+            <div class="col-md-3">
+                <div class="mt-md" id="fixa-menu-eixo">
+                    <div class="menu-submenu" id="menueixos-sticker">
+                        <div class="panel panel-submenu">
+                            <div class="panel-heading">
+                                <h6 class="panel-title">Filtrar Pautas</h6>
+                            </div>
+                            <div class="panel-body">
+                                <div class="filtro">
+                                    <form>
+                                        <ul class="status">
+                                            <?php foreach (get_terms('situacao') as $situacao) : ?>
+                                            <li>
+                                                <span
+                                                class="<?php echo (isset($_GET['situacao_filtro'][$situacao->slug]) && $_GET['situacao_filtro'][$situacao->slug] == 'on') ? 'selected' : ''; ?>">
+                                                <?php echo $situacao->name; ?>
+                                                <input type="hidden" name="situacao_filtro[<?php echo $situacao->slug; ?>]"
+                                                value="<?php echo (isset($_GET['situacao_filtro'][$situacao->slug]) && $_GET['situacao_filtro'][$situacao->slug] == 'on') ? 'on' : ''; ?>"/>
+                                                </span>
+                                            </li>
+                                            <?php endforeach; ?>
+                                        </ul>
+                                        <ul>
+                                            <?php foreach (get_terms('tema') as $tema) : ?>
+                                            <li>
+                                                <label class="checkbox">
+                                                    <input type="checkbox"
+                                                    name="tema_filtro[<?php echo $tema->slug; ?>]" <?php echo (isset($_GET['tema_filtro'][$tema->slug]) && $_GET['tema_filtro'][$tema->slug] == 'on') ? ' checked="checked" ' : ''; ?> />
+                                                    <?php echo $tema->name; ?>
+                                                </label>
+                                            </li>
+                                            <?php endforeach; ?>
+                                        </ul>
+                                        <div class="textright">
+                                            <button type="submit" class="btn">Filtrar</button>
+                                        </div>
+                                    </form>
+                                    <?php
+                                    endif;
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </form>
-                <?php
-                endif;
-                ?>
-
+                </div>
+            </div>
+            <div class="col-md-9">
                 <div class="mt-lg divider-bottom">
                     <h5 class="font-roboto red"><strong>Lista de pautas sendo discutidas nesse tema</strong></h5>
                 </div>
@@ -96,7 +115,6 @@ if ( is_object($tema) && property_exists($tema, 'taxonomy') && $tema->taxonomy =
                     <a href="<?php echo site_url('nova-pauta'); ?>" class="btn btn-danger">Criar uma nova pauta</a>
                     <?php endif; ?>
                 </div>
-                
             </div>
         </div>
     </div>
