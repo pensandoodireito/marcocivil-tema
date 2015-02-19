22e4da0093dd984f2e36edf7bab999026a892656
Marco Antonio Konopacki <marcoamarelo@gmail.com>
2015-02-19 17:07:37 -0300

    inclusão da função para inclusão de metas especiais no header do site

--- functions.php
+++ functions.php
@@ -1,2 +1,11 @@
 <?php
+/**
+ * Função para incluir metas no header do marcocivil
+ */
+function marcocivil_meta_headers() {
+    ?>
+    <meta name="google-site-verification" content="XUlRiKgckbM8CvDw1o9A-anhRvEMgzeXK5yngPXmePo" />
+<?php
+}
 
+add_action( 'wp_head', 'marcocivil_meta_headers' );
