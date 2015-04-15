1145a5887a7557c3f38b472d37c692d0051aabbc
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2015-04-15 14:23:49 -0300

    Adicionando campo TAGs para os comentários #61 Opção disponível apenas na área de administração. FIX pensandoodireito/participacao-sitebase#61

--- functions.php
+++ functions.php
@@ -44,3 +44,68 @@ function autor_exibicao_save( $post_id )
     }
 
 }
+
+//Adicionando campo de TAG nos comentários.
+//Baseado em: http://wordpress.stackexchange.com/questions/82317/show-custom-comment-fields-when-editing-in-admin
+// SAVE COMMENT META
+// only found this hook to process the POST
+add_filter( 'comment_edit_redirect',  'save_comment_wpse_82317', 10, 2 );
+
+// META BOX
+add_action( 'add_meta_boxes', 'add_custom_box_wpse_82317' );
+
+/**
+ * Save Custom Comment Field
+ * This hook deals with the redirect after saving, we are only taking advantage of it
+ */
+function save_comment_wpse_82317( $location, $comment_id )
+{
+    // Not allowed, return regular value without updating meta
+    if ( !wp_verify_nonce( $_POST['noncename_wpse_82317'], plugin_basename( __FILE__ ) ) 
+        && !isset( $_POST['meta_comment_field'] ) 
+        ) 
+        return $location;
+
+    // Convert text field into array splitted by comma
+    $tags = array_map( 'trim', explode(',', $_POST['meta_tag_field']) ); 
+    $tags = array_map( 'sanitize_text_field', $tags); 
+
+    // Update meta
+    update_comment_meta( 
+        $comment_id, 
+        'meta_tag_field', 
+        implode(',', $tags) 
+    );
+
+    // Return regular value after updating  
+    return $location;
+}
+
+/**
+ * Add Comment meta box 
+ */
+function add_custom_box_wpse_82317() 
+{
+    add_meta_box( 
+        'section_id_wpse_82317',
+        __( 'Tags' ),
+        'inner_custom_box_wpse_82317',
+        'comment',
+        'normal',
+        'high'
+    );
+}
+
+/**
+ * Render meta box with Custom Field 
+ */
+function inner_custom_box_wpse_82317( $comment ) 
+{
+    // Use nonce for verification
+    wp_nonce_field( plugin_basename( __FILE__ ), 'noncename_wpse_82317' );
+
+    $c_meta = get_comment_meta( $comment->comment_ID, 'meta_tag_field', true );
+    echo "<input type='text' id='meta_tag_field' name='meta_tag_field' value='", 
+        esc_attr( $c_meta ), 
+        "' size='25' />";
+}
