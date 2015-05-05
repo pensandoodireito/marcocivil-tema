7d28720e719f41248d5995024339ad8298d7042e
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-05-05 19:14:09 -0300

    Criação da página page-debate-em-numeros.php;

---
+++ page-debate-em-numeros.php
@@ -0,0 +1,85 @@
+<?php get_header(); ?>
+<div id="marco-numeros">
+    <div class="top">
+        <div class="container text-center">
+            <div class="logo">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-half-branca.png" class="img-adptive"
+                alt="Logo: Marco Civil da Internet">
+                <p class="white font-roboto h1"><strong>O debate em números</strong></p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row text-center">
+            <h3 class="h1 red font-roboto">Pautas por eixo</h3>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="well well-sm">
+                    <div class="row">
+                        <div class="col-md-4 text-center">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/privacidade-na-rede.png" class="img-adptive">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="font-roboto red h1">0000</p>
+                            <p><strong>Privacidade na rede</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="well well-sm">
+                    <div class="row">
+                        <div class="col-md-4 text-center">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/registros-de-acesso.png" class="img-adptive">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="font-roboto red h1">0000</p>
+                            <p><strong>Registros de acesso</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="well well-sm">
+                    <div class="row">
+                        <div class="col-md-4 text-center">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/neutralidade.png" class="img-adptive">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="font-roboto red h1">0000</p>
+                            <p><strong>Neutralidade</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="well well-sm">
+                    <div class="row">
+                        <div class="col-md-4 text-center">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/outros-temas-e-consideracoes.png" class="img-adptive">
+                        </div>
+                        <div class="col-md-8">
+                            <p class="font-roboto red h1">0000</p>
+                            <p><strong>Outros temas</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="criacao-pautas">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    grafico
+                </div>
+                <div class="col-md-6 text-right font-roboto">
+                    <p><strong class="red h1">339</strong><span class="h3"> pautas criadas</span></p>
+                    <p><span class="h3">durante</span><strong class="red h1">90</strong></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
