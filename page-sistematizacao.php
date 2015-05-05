86cc7825a4a69b640745feaec531be07ced52dc5
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-05-05 11:47:42 -0300

    Pagina sistematização

---
+++ page-sistematizacao.php
@@ -0,0 +1,147 @@
+<?php get_header(); ?>
+<div class="container">
+    <h2 class="font-roboto red">Sistematização do Marco Civil</h2>
+    <p>
+        Praesent est turpis, ullamcorper eu porta eu, gravida non ex. Nulla faucibus laoreet tortor, at luctus nisl mollis sed.
+    </p>
+</div>
+<div id="sistematizacao">
+    <div class="sist-top">
+        <div class="sist-base">
+            <div class="container sist-content">
+                <div class="sist-logo">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sistematizacao-marcocivil.png" class="img-adptive"
+                    alt="Logo: Marco Civil da Internet">
+                </div>
+                <div class="sist-menu col-md-offset-6 col-md-5">
+                    <ul class="list-unstyled">
+                        <li><a href="#">O que é sistematização?</a></li>
+                        <li><a href="#">Como participar?</a></li>
+                        <li><a href="#">Ferramentas de colaboração</a></li>
+                        <li><a href="#">Eixos do debate</a></li>
+                        <li><a href="#">Envie seu decreto</a></li>
+                    </ul>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    <div class="container">
+        <div class="oque clearfix mt-lg">
+            <div class="ml-md col-md-6">
+                <h2 class="font-roboto red h1">O que é?</h2>
+                <p>O Marco Civil da Internet, Lei 12.965/14, estabelece princípios, garantias, direitos e deveres dos usuários da Internet no Brasil, sendo a primeira lei construída de forma colaborativa entre Governo e sociedade utilizando a internet como plataforma de debate.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum, lacus vitae eleifend aliquet, leo lacus consequat elit, sed sollicitudin mauris massa vel erat. </p>
+                <p>Quisque ultricies laoreet orci. Etiam eros elit, consectetur a enim nec, dapibus placerat neque. Duis sed erat urna. Fusce auctor elit vitae massa posuere tempor. Nunc elementum sollicitudin felis sed euismod. Nam vulputate, arcu porta pretium lacinia, est lorem gravida massa, ac faucibus quam neque nec dui. Duis accumsan rutrum dolor, a lacinia erat gravida non</p>
+            </div>
+            <div class="col-md-5 text-center">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cubo.jpg" class="img-adptive"
+                alt="Logo: Marco Civil da Internet">
+            </div>
+        </div>
+    </div>
+    <div class="comoparticipar">
+        <div class="container">
+            <div class="row">
+                <div class="ml-md col-md-6">
+                    <h2 class="font-roboto red h1">Como participar</h2>
+                    <p class="mt-md">O Marco Civil da Internet, Lei 12.965/14, estabelece princípios, garantias, direitos e deveres dos usuários da Internet no Brasil, sendo a primeira lei construída de forma colaborativa entre Governo e sociedade utilizando a internet como plataforma de debate.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum, lacus vitae eleifend aliquet, leo lacus consequat elit, sed sollicitudin mauris massa vel erat. </p>
+                    <p>Quisque ultricies laoreet orci. Etiam eros elit, consectetur a enim nec, dapibus placerat neque. Duis sed erat urna. Fusce auctor elit vitae massa posuere tempor. Nunc elementum sollicitudin felis sed euismod. Nam vulputate, arcu porta pretium lacinia, est lorem gravida massa, ac faucibus quam neque nec dui. Duis accumsan rutrum dolor, a lacinia erat gravida non</p>
+                </div>
+                <div class="col-md-5 menu-participar">
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#" class="red">Download do histórico</a>
+                            <p><small>Eixos, comentários etc ( em fomato “.cvs”)</small></p>
+                        </li>
+                        <li><a href="#">Histórico online</a></li>
+                        <li><a href="#">Envie seu decreto</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ferr-cont">
+        <div class="container">
+            <h2 class="font-roboto red h1 mb-md">Ferramentas de contribuição</h2>
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bck-oquee.jpg" class="img-adptive"
+                    alt="Logo: Marco Civil da Internet">
+                </div>
+                <div class="ml-md col-md-6">
+                    <h4 class="font-roboto red">Como participar</h4>
+                    <p class="mt-md"><small><a href="#">Link</a></small></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum, lacus vitae eleifend aliquet, leo lacus consequat elit, sed sollicitudin mauris massa vel erat. </p>
+                    <p>Quisque ultricies laoreet orci. Etiam eros elit, consectetur a enim nec, dapibus placerat neque. Duis sed erat urna. Fusce auctor elit vitae massa posuere tempor. Nunc elementum sollicitudin felis sed euismod. Nam vulputate, arcu porta pretium lacinia, est lorem gravida massa, ac faucibus quam neque nec dui. Duis accumsan rutrum dolor, a lacinia erat gravida non</p>
+                </div>
+            </div>
+            <div class="row divider-top">
+                <div class="col-md-4 text-center">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bck-oquee.jpg" class="img-adptive"
+                    alt="Logo: Marco Civil da Internet">
+                </div>
+                <div class="ml-md col-md-6">
+                    <h4 class="font-roboto red">Como participar</h4>
+                    <p class="mt-md"><small><a href="#">Link</a></small></p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum, lacus vitae eleifend aliquet, leo lacus consequat elit, sed sollicitudin mauris massa vel erat. </p>
+                    <p>Quisque ultricies laoreet orci. Etiam eros elit, consectetur a enim nec, dapibus placerat neque. Duis sed erat urna. Fusce auctor elit vitae massa posuere tempor. Nunc elementum sollicitudin felis sed euismod. Nam vulputate, arcu porta pretium lacinia, est lorem gravida massa, ac faucibus quam neque nec dui. Duis accumsan rutrum dolor, a lacinia erat gravida non</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="eixos">
+        <div class="container">
+            <h4 class="font-roboto red h1">Eixos</h4>
+            <div class="row">
+                <?php
+                $temas = get_terms( 'tema', 'orderby=id&hide_empty=0' );
+                foreach($temas as $tema): ?>
+                
+                <div class="col-sm-3 text-center">
+                    <a href="<?php echo get_term_link($tema); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/<?php echo $tema->slug; ?>.png"
+                    class="img-adptive" alt=""></a>
+                    <h5 class="font-roboto blue"><strong>
+                    <a href="<?php echo get_term_link($tema); ?>"><?php echo $tema->name; ?></a>
+                    </strong></h5>
+                </div>
+                
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+    <div class="envie">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h4 class="font-roboto red h1">Envie seu decreto</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rutrum, lacus vitae eleifend aliquet, leo lacus consequat elit, sed sollicitudin mauris massa vel erat. </p>
+                    <p>Quisque ultricies laoreet orci. Etiam eros elit, consectetur a enim nec, dapibus placerat neque. Duis sed erat urna. Fusce auctor elit vitae massa posuere tempor. Nunc elementum sollicitudin felis sed euismod. Nam vulputate, arcu porta pretium lacinia, est lorem gravida massa, ac faucibus quam neque nec dui. Duis accumsan rutrum dolor, a lacinia erat gravida non</p>
+                </div>
+                <div class="col-md-6">
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputFile">Selecione o arquivo</label>
+                            <input type="file" id="exampleInputFile">
+                            <p class="help-block">Example block-level help text here.</p>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox"> Check me out
+                            </label>
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_template_part('front','noticias'); ?>
+<?php get_footer(); ?>
