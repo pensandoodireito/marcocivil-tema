78b038b7fabf541bbef1ceaba3e0bc28dd325355
Thiago Lopes <thiago.sousa.lopes@gmail.com>
2015-05-05 20:29:12 -0300

    ajustes na tela debate em numeros

--- page-debate-em-numeros.php
+++ page-debate-em-numeros.php
@@ -21,7 +21,7 @@
                             <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/privacidade-na-rede.png" class="img-adptive">
                         </div>
                         <div class="col-md-8">
-                            <p class="font-roboto red h1">0000</p>
+                            <p class="font-roboto red h1 text-center">68</p>
                             <p><strong>Privacidade na rede</strong></p>
                         </div>
                     </div>
@@ -34,7 +34,7 @@
                             <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/registros-de-acesso.png" class="img-adptive">
                         </div>
                         <div class="col-md-8">
-                            <p class="font-roboto red h1">0000</p>
+                            <p class="font-roboto red h1 text-center">70</p>
                             <p><strong>Registros de acesso</strong></p>
                         </div>
                     </div>
@@ -47,7 +47,7 @@
                             <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/neutralidade.png" class="img-adptive">
                         </div>
                         <div class="col-md-8">
-                            <p class="font-roboto red h1">0000</p>
+                            <p class="font-roboto red h1 text-center">98</p>
                             <p><strong>Neutralidade</strong></p>
                         </div>
                     </div>
@@ -60,7 +60,7 @@
                             <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/temas/outros-temas-e-consideracoes.png" class="img-adptive">
                         </div>
                         <div class="col-md-8">
-                            <p class="font-roboto red h1">0000</p>
+                            <p class="font-roboto red h1 text-center">124</p>
                             <p><strong>Outros temas</strong></p>
                         </div>
                     </div>
@@ -70,13 +70,103 @@
     </div>
     <div class="criacao-pautas">
         <div class="container">
-            <div class="row">
+            <div class="row text-center">
+                <h3 class="h1 red font-roboto">Criação de pautas ao longo dos debates</h3>
+            </div>
+            <div class="row mt-md">
                 <div class="col-md-6">
-                    grafico
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/grafico-pautas.png" class="img-adptive">
                 </div>
-                <div class="col-md-6 text-right font-roboto">
-                    <p><strong class="red h1">339</strong><span class="h3"> pautas criadas</span></p>
-                    <p><span class="h3">durante</span><strong class="red h1">90</strong></p>
+                <div class="col-md-6 text-left font-roboto">
+                    <p class="mt-md h3"><strong class="red h1">339 </strong>pautas criadas</p>
+                    <p class="mt-md h3">Durante <strong class="red h1">90 </strong>dias de debate</p>
+                    <p class="mt-md h3"><strong class="red h1">1200 </strong>comentários</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="acessos">
+        <div class="container">
+            <div class="row text-center">
+                <h3 class="h1 red font-roboto">Acessos ao site</h3>
+            </div>
+            <div class="row mt-md">
+                <div class="col-sm-3 col-sm-offset-3 text-left font-roboto">
+                    <p class="mt-md h3"><strong class="red h1">48.823 </strong>visitas únicas</p>
+                    <p class="mt-md h3"><strong class="red h1">61.333 </strong>visitas totais</p>
+                    <p class="mt-md h3"><strong class="red h1">192.706 </strong>pageviews</p>
+                </div>
+                <div class="col-sm-4">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/acesso-site.png" class="img-adptive">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="redes-sociais">
+        <div class="container">
+            <div class="row text-center">
+                <h3 class="h1 red font-roboto">Nas redes sociais</h3>
+            </div>
+            <div class="row mt-md">
+                <div class="col-md-4">
+                    <div class="well well-sm">
+                        <div class="text-center">
+                            <i class="fa fa-facebook fa-5x"></i>
+                        </div>
+                        <div class="mt-md">
+                            <p class="mt-md h3"><strong class="red h1 font-roboto">1200 </strong>seguidores</p>
+                            <p class="mt-md h3"><strong class="red h1 font-roboto">1200 </strong>seguidores</p>
+                            <p class="mt-md h3"><strong class="red h1 font-roboto">1200 </strong>seguidores</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="well well-sm">
+                        <div class="text-center">
+                            <i class="fa fa-twitter fa-5x"></i>
+                        </div>
+                        <div class="mt-md">
+                            <p class="mt-md h3"><strong class="red h1 font-roboto">1200 </strong>seguidores</p>
+                            <p class="mt-md h3"><strong class="red h1 font-roboto">1200 </strong>seguidores</p>
+                            <p class="mt-md h3"><strong class="red h1 font-roboto">1200 </strong>seguidores</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="well well-sm">
+                        <div class="text-center">
+                            <i class="fa fa-youtube fa-5x"></i>
+                        </div>
+                        <div class="mt-md">
+                            <p class="mt-md h3"><strong class="red h1 font-roboto">1200 </strong>seguidores</p>
+                            <p class="mt-md h3"><strong class="red h1 font-roboto">1200 </strong>seguidores</p>
+                            <p class="mt-md h3"><strong class="red h1 font-roboto">1200 </strong>seguidores</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mapas">
+        <div class="container">
+            <div class="row text-center">
+                <h3 class="h1 red font-roboto">Localização dos participantes</h3>
+            </div>
+            <div class="row mt-md">
+                <div class="col-md-12 text-center">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/grafico-mapa.png" class="img-adptive">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="palavras">
+        <div class="container">
+            <div class="row text-center">
+                <h3 class="h1 red font-roboto">As palavras mais usadas no debate</h3>
+            </div>
+            <div class="row mt-md">
+                <div class="col-md-12 text-center">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/nuvem-palavras.png" class="img-adptive">
                 </div>
             </div>
         </div>
