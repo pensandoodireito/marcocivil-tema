2ab03a0fb6d3c3077a7b891a2194b3284c001ca8
Josafá Filho <josafafilho15@gmail.com>
2015-10-27 19:30:00 -0200

    Ajuste na formatação do código

--- delibera/js/creta-votacao.js
+++ delibera/js/creta-votacao.js
@@ -1,5 +1,5 @@
-jQuery(document).ready(function() {
-    jQuery('#submit').click(function() {
+jQuery(document).ready(function () {
+    jQuery('#submit').click(function () {
         if (jQuery('#delibera_commentform input[type=checkbox]:checked').length == 0) {
             jQuery('#nenhum-voto').show('slow').delay(5000).hide('slow');
             return false;
